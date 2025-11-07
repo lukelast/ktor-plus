5cf5744e3f0da3442b975a70ef224bd69bc19591
Luke <luke@lukelast.com>
2025-11-07 10:54:13 -0500

    Is winner?

---
+++ libs/ktp-core/src/main/kotlin/net/ghue/ktp/log/IsGcloudRun.kt
@@ -0,0 +1,14 @@
+package net.ghue.ktp.log
+
+import ch.qos.logback.core.boolex.PropertyConditionBase
+
+class IsGcloudRun : PropertyConditionBase() {
+    override fun evaluate(): Boolean {
+        val isGcloudRun =
+            System.getenv("K_SERVICE") != null || System.getProperty("K_SERVICE") != null
+        if (isGcloudRun) {
+            log {}.info { "Determined to be running in Google Cloud Run" }
+        }
+        return isGcloudRun
+    }
+}
