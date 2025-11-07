df861bc4e0daf3dd1ca7ebbd5890c32032bb2352
Luke <luke@lukelast.com>
2025-11-07 18:46:09 -0500

    add some util functions

--- libs/ktp-ktor/src/main/kotlin/net/ghue/ktp/ktor/plugin/Util.kt
+++ libs/ktp-ktor/src/main/kotlin/net/ghue/ktp/ktor/plugin/Util.kt
@@ -1,6 +1,16 @@
 package net.ghue.ktp.ktor.plugin
 
 import io.ktor.http.*
+import io.ktor.server.plugins.origin
+import io.ktor.server.routing.RoutingCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.slf4j.MDCContext
+import kotlinx.coroutines.withContext
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 import kotlin.time.Duration
 
 fun cacheControlMaxAge(
@@ -12,3 +22,19 @@ fun cacheControlMaxAge(
         visibility = visibility,
     )
 }
+
+fun RoutingCall.originUrl(path: List<String> = emptyList()): Url = buildUrl {
+    protocol = URLProtocol.createOrDefault(request.origin.scheme)
+    host = request.origin.serverHost
+    port = request.origin.serverPort
+    pathSegments = path
+}
+
+@OptIn(ExperimentalContracts::class)
+suspend inline fun <T> withIoContext(noinline block: suspend CoroutineScope.() -> T): T {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    return withContext(context = Dispatchers.IO + MDCContext(), block = block)
+}
+
+
+
