4472a1b0e0f14cb860ac77928d98ba6ce2d89cb6
Luke <luke@lukelast.com>
2025-11-26 11:43:29 -0500

    update versions

--- ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/project/mods/UnitTesting.kt
+++ ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/project/mods/UnitTesting.kt
@@ -12,7 +12,8 @@ fun Project.installKotest() {
         add("testImplementation", platform("io.kotest:kotest-bom:${Version.KOTEST}"))
         add("testImplementation", "io.kotest:kotest-runner-junit5")
         add("testImplementation", "io.kotest:kotest-assertions-core")
-        add("testImplementation", "io.kotest:kotest-extensions-koin")
+        // Version isn't in the BOM so must be set.
+        add("testImplementation", "io.kotest:kotest-extensions-koin:6.0.4", )
         add("testImplementation", "io.kotest:kotest-assertions-ktor")
     }
     project.tasks.withType<Test> {
--- libs/ktp-test/build.gradle.kts
+++ libs/ktp-test/build.gradle.kts
@@ -3,6 +3,7 @@ plugins { id("com.github.lukelast.ktor-plus") }
 dependencies {
     api(project(":libs:ktp-ktor"))
 
+    api(platform(libs.kotest.bom))
     api(libs.mockk)
     api(libs.ktor.test)
     api(libs.koinTest)
