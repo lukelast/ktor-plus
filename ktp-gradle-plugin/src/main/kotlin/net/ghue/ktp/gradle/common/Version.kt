00fc437adf060818768363d7d2597e7f6a0ba8d8
Luke <luke@lukelast.com>
2026-02-18 23:35:38 -0500

    Updates dependencies
    Updates various dependencies to their latest versions, including Kotlin, Koin, Ktor, Kotest, and others.
    This ensures the project benefits from the latest features, bug fixes, and performance improvements in these libraries.

--- ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/common/Version.kt
+++ ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/common/Version.kt
@@ -1,7 +1,7 @@
 package net.ghue.ktp.gradle.common
 
 object Version {
-    const val KOTEST = "6.0.5"
-    const val KOIN = "4.1.0"
-    const val KOIN_KSP = "2.1.0"
+    const val KOTEST = "6.1.3"
+    const val KOIN = "4.1.1"
+    const val KOIN_KSP = "2.3.1"
 }
