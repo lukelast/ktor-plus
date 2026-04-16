29741dd5b403a68872fb73b650bfe96a0e90b177
Luke <luke@lukelast.com>
2026-04-16 19:09:50 -0400

    Disable Koin compiler plugin compileSafety in Gradle configuration
    Turn off the Koin compiler's compileSafety flag when applying the plugin to avoid strict compile-time checks that can cause build issues and to align plugin behavior with the project configuration.

--- ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/project/mods/Koin.kt
+++ ktp-gradle-plugin/src/main/kotlin/net/ghue/ktp/gradle/project/mods/Koin.kt
@@ -1,8 +1,11 @@
 package net.ghue.ktp.gradle.project.mods
 
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.koin.compiler.plugin.KoinGradleExtension
 import org.koin.compiler.plugin.KoinGradlePlugin
 
 fun Project.applyKoinCompilerPlugin() {
     pluginManager.apply(KoinGradlePlugin::class.java)
+    configure<KoinGradleExtension> { compileSafety.set(false) }
 }
